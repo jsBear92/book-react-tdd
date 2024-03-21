c8a7d0b4972156e2ccdf29ecd8fc578992a87a2e
Jaeseong Jeong <js104427@gmail.com>
2024-03-21 22:35:07 +0800

    extend stub book service with middleware

--- cypress/e2e/book.spec.cy.ts
+++ cypress/e2e/book.spec.cy.ts
@@ -1,18 +1,41 @@
-describe('Book Application', () => {
-    it('Visits the Book', () => {
-        cy.visit('http://localhost:3000/');
-        cy.get('h2[data-test="heading"]').contains('Book')
-    })
-    it('Shows a book list', () => {
-        cy.visit('http://localhost:3000/');
-        cy.get('div[data-test="book-list"]').should('exist');
-        cy.get('div.book-item').should((books) => {
-            expect(books).to.have.length(2);
+import axios from "axios";
 
-            const titles = [...books].map(x => x.querySelector('h2').innerHTML);
-            expect(titles).to.deep.equal(
-                ['Refactoring', 'Domain-driven design']
-            )
-        })
-    })
-})
+describe("Book Application", () => {
+  before(() => {
+    return axios
+      .delete("http://localhost:8080/books?_cleanup=true")
+      .catch((err) => err);
+  });
+  afterEach(() => {
+    return axios
+      .delete("http://localhost:8080/books?_cleanup=true")
+      .catch((err) => err);
+  });
+  beforeEach(() => {
+    const books = [
+      { name: "Refactoring", id: 1 },
+      { name: "Domain-driven design", id: 2 },
+      { name: "Building Microservices", id: 3 }
+    ];
+
+    return books.map((item) =>
+      axios.post("http://localhost:8080/books", item, {
+        headers: { "Content-Type": "application/json" },
+      })
+    );
+  });
+  it("Visits the Book", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('h2[data-test="heading"]').contains("Book");
+  });
+  it("Shows a book list", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('div[data-test="book-list"]').should("exist");
+    cy.get("div.book-item").should((books) => {
+      expect(books).to.have.length(3);
+
+      const titles = [...books].map((x) => x.querySelector("h2").innerHTML);
+      expect(titles).to.deep.equal(["Refactoring", "Domain-driven design", "Building Microservices"]);
+    });
+  });
+});
