e3d21ff3c43c7fad7536d880866292ada4e87c7b
Jaeseong Jeong <js104427@gmail.com>
2024-03-21 21:53:43 +0800

    talk to backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import BookList from './BookList';
+import BookList from "./BookList";
+import axios from "axios";
+import type { Book } from "./types";
 import { Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 
 function App() {
-  const books = [
-    { name: 'Refactoring' }, { name: 'Domain-driven design'}
-  ];
+  const [books, setBooks] = useState<Book[]>([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/books").then((res) => setBooks(res.data));
+  }, []);
   return (
     <>
       <Typography variant="h2" component="h2" data-test="heading">
