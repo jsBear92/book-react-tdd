cc887883d1db2df98f88201825e577275743c31e
Jaeseong Jeong <js104427@gmail.com>
2024-03-21 20:37:17 +0800

    refactoring - extract function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
-import { Typography } from '@mui/material';
+import type {Book} from './types';
+import { Typography } from "@mui/material";
+
+const renderBooks = (books: Book[]) => {
+  return <div data-test='book-list'>
+    {books.map(book => (
+          <div className="book-item">
+            <h2 className="title">{book.name}</h2>
+          </div>
+        ))}
+  </div>
+}
 
 function App() {
+  const books = [
+    { name: 'Refactoring' }, { name: 'Domain-driven design'}
+  ];
   return (
-    <Typography variant="h2" component="h2" data-test="heading">
-      Book
-    </Typography>
+    <>
+      <Typography variant="h2" component="h2" data-test="heading">
+        Book
+      </Typography>
+      {renderBooks(books)}
+    </>
   );
 }
 
